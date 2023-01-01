f98488df53e0d82037b017e974b00c12af84a4c8
AD <artyukhindmitry@yandex.ru>
2023-01-01 11:54:05 +0400

    dz25

---
+++ dz25/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.WriteLine("введи N");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + ", " + PrintNumbers(start - 1, end));
+// }
+// Console.WriteLine(PrintNumbers(n, 1));
+
+
+
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("введи М");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введи N");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// string PrintSum(int start, int end)
+// {
+//     sum += start;
+//     if (start == end) return sum.ToString();
+    
+//     return (PrintSum(start + 1, end));
+// }
+// Console.WriteLine(PrintSum(m, n));
+
+
+
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.WriteLine("введи М");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введи N");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int Acer(int m, int n)
+// {
+//   if (m == 0)
+//     return n + 1;
+//   else
+//     if ((m != 0) && (n == 0))
+//       return Acer(m - 1, 1);
+//     else
+//       return Acer(m - 1, Acer(m, n - 1));
+// }
+// Console.WriteLine(Acer(m, n));
---
+++ sem25/Program.cs
@@ -0,0 +1,100 @@
+﻿// задача цикла (другой метод)
+
+// int rows = 2;
+// int columns = 2;
+// int[,] array = new int[2,2]; // 2 строчки, 2 столбца
+
+// for (int i = 0; i < rows * columns; i++)
+// {
+//     array[i / columns, i % columns] = new Random().Next(11);
+// }
+
+
+
+
+
+
+// Рекурсия
+
+// int Factorial(int num) // num - число, от которого ищется факториал
+// {
+//     // Базовый случай - выходите из рекурсии, рекурсия = бесконечный цикл без базового случая
+//     if (num == 1)
+//     {
+//         return 1;
+//     }
+//     // Рекурсивный случай
+//     // num = 5, 5 * 4 * 3 * 2 * 1
+//     return num * Factorial(num - 1);
+// }
+
+// Console.WriteLine(Factorial(4)); // 4! = 1 * 2 * 3 * 4 = 24
+
+
+
+
+// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// int n = Convert.ToInt32(Console.ReadLine()); // N (правая граница)
+// // 1; N: start = 1, end = N
+// string PrintNumbers(int start, int end)
+// {
+//     // Базовый случай - выход из рекурсии
+//     if (start == end) return start.ToString(); // n = 2: res = 1, 2(start == end)
+//     // Рекурсивный случай
+
+//     return (start + ", " + PrintNumbers(start + 1, end));
+// }
+
+// Console.WriteLine(PrintNumbers(1, n));
+
+
+
+
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// int digit = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumbers(int number)
+// {
+//     // Базовый случай
+//     if (number == 0) return 0;
+//     // Рекурсивный случай
+//     return (number % 10 + SumNumbers(number / 10));
+// }
+
+// Console.WriteLine($"Сумма цифр в числе {digit} = {SumNumbers(digit)}");
+
+
+
+
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите cтепень: ");
+// int degree = Convert.ToInt32(Console.ReadLine());
+
+// int Pow(int digit, int power)
+// {
+//     if (power == 0) return 1; 
+//     if (power == 1) return digit;
+//     // Рекурсивный случай
+//     return (digit * Pow(digit, power - 1)); 
+//     // power - грубо говоря, счетчик 
+// }
+
+
+
+
+
+
